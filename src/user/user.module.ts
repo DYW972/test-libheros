a74ac1946d22f2f940d7f9fc0a83eb1572ba24e4
Yohan William DUNON <yoh.d.tech@proton.me>
2025-05-30 01:02:39 +0200

    [chore]: Update user module
    - Update class names
    - Export user service to use it later if needed
    
    [Issue: #]

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './user.controller';
-import { UsersService } from './user.service';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
 import { User } from './user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  controllers: [UsersController],
-  providers: [UsersService],
+  controllers: [UserController],
+  providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {}
