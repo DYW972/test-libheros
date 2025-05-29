a1f322c5ed9fae67cbc5bdc1dcb9a3ed6654856c
Yohan William DUNON <yoh.d.tech@proton.me>
2025-05-29 23:54:37 +0200

    [feat]: Add user service
    - Implement user service with CRUD methods and user login by email or username
    
    [Issue: #]

---
+++ src/user/user.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  findAll() {
+    return this.repo.find();
+  }
+
+  findOne(id: string) {
+    return this.repo.findOne({ where: { id } });
+  }
+
+  create(userData: Partial<User>) {
+    const user = this.repo.create(userData);
+    return this.repo.save(user);
+  }
+
+  async update(id: string, attrs: Partial<User>) {
+    await this.repo.update(id, attrs);
+    return this.findOne(id);
+  }
+
+  delete(id: string) {
+    return this.repo.delete(id);
+  }
+}
