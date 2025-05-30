6bd8c185f717e0550bd555ea8f1c2290aab31c4e
Yohan William DUNON <yoh.d.tech@proton.me>
2025-05-30 20:02:08 +0200

    [chore]: Update modules code structure
    - Ordering the imports entities
    
    [Issue: #]

--- src/tasks-list/tasks-list.module.ts
+++ src/tasks-list/tasks-list.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { TasksList } from './tasks-list.entity';
-import { TasksListController } from './tasks-list.controller';
 import { TasksListService } from './tasks-list.service';
 import { TasksListGateway } from './tasks-list.gateway';
+import { TasksListController } from './tasks-list.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([TasksList])],
