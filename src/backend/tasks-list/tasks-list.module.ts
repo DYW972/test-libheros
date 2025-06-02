470286762149a8a18e067229287a26d5a23e0bb1
Yohan William DUNON <yoh.d.tech@proton.me>
2025-06-02 19:25:41 +0200

    [chore]: Update unused imports and instances
    -
    
    [Issue: #]

--- src/backend/tasks-list/tasks-list.module.ts
+++ src/backend/tasks-list/tasks-list.module.ts
@@ -3,13 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { TasksList } from './tasks-list.entity';
 import { TasksListService } from './tasks-list.service';
-import { TasksListGateway } from './tasks-list.gateway';
 import { TasksListController } from './tasks-list.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([TasksList])],
   controllers: [TasksListController],
-  providers: [TasksListService, TasksListGateway],
+  providers: [TasksListService],
   exports: [TasksListService],
 })
 export class TasksListModule {}
--- src/backend/user/user.module.ts
+++ src/backend/user/user.module.ts
@@ -4,12 +4,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
-import { UserGateway } from './user.gateway';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
-  providers: [UserService, UserGateway],
+  providers: [UserService],
   exports: [UserService],
 })
 export class UserModule {}
