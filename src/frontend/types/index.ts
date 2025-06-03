9aa22796cf50dd3dc47e3d12216791a86bc2f1ec
Yohan William DUNON <yoh.d.tech@proton.me>
2025-06-03 10:41:33 +0200

    [refactor]: Optimize types exports
    - Create an index file in the types directory to optimize their import in components
    
    [Issue: #]

---
+++ src/frontend/types/index.ts
@@ -0,0 +1,25 @@
+import { TUser } from './User';
+import { TTask } from './Task';
+import { TInput } from './Input';
+import { TModalProps } from './Modal';
+import { TTasksList } from './TasksList';
+import { TTaskCreateProps } from './TaskCreate';
+import { TUseFormReturn } from './useFormReturn';
+import { TResponseError } from './ResponseError';
+import { TTaskCreateFormProps } from './TaskCreateForm';
+import { TTasksListSideBarProps } from './TasksListSidebar';
+import { TaskDetailsSidebarProps } from './TaskDetailsSidebar';
+
+export type {
+  TUser,
+  TTask,
+  TInput,
+  TTasksList,
+  TModalProps,
+  TResponseError,
+  TUseFormReturn,
+  TTaskCreateProps,
+  TTaskCreateFormProps,
+  TTasksListSideBarProps,
+  TaskDetailsSidebarProps,
+};
