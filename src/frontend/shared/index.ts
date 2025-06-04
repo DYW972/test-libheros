731d47b789d99f4dfcc7b68a272b24594ba03a77
Yohan William DUNON <yoh.d.tech@proton.me>
2025-06-04 12:10:24 +0200

    [feat]: Update index file in shared directory
    - Update import syntax to namespace import to import all exported members from the target module into a single object
    - Update exported object with namespaces: Hooks, Components, Types, Schemas, Functions, Interfaces
    
    [Issue: #]

--- src/frontend/shared/index.ts
+++ src/frontend/shared/index.ts
@@ -1,4 +1,8 @@
-import { useForm } from './hooks';
-import { Input } from './components';
+import * as Hooks from './hooks';
+import * as Types from './types';
+import * as Schemas from './schemas';
+import * as Components from './components';
+import * as Functions from './functions';
+import * as Interfaces from './interfaces';
 
-export { useForm, Input };
+export { Hooks, Components, Types, Schemas, Functions, Interfaces };
