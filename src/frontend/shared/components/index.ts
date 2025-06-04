cc7af67ca008737c78c682233c680b405e7f07c0
Yohan William DUNON <yoh.d.tech@proton.me>
2025-06-04 13:00:11 +0200

    [feat]: Update index file in components directory
    - Add import and export for the new component RegisterForm
    
    [Issue: #]

--- src/frontend/shared/components/index.ts
+++ src/frontend/shared/components/index.ts
@@ -1,5 +1,6 @@
 import Input from './Input';
 import LoginForm from './LoginForm';
+import RegisterForm from './RegisterForm';
 import ClickToAction from './ClickToAction';
 
-export { Input, ClickToAction, LoginForm };
+export { Input, ClickToAction, LoginForm, RegisterForm };
