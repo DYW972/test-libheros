0aa4475ddcb290733fd70aed6f92f5b5a2ffd8bf
Yohan William DUNON <yoh.d.tech@proton.me>
2025-06-04 11:55:45 +0200

    [feat]: Update index file in Types directory
    - Add import and export for new types THttpMethod, TLoginFormProps and TClickToActionProps
    
    [Issue: #]

--- src/frontend/shared/types/index.ts
+++ src/frontend/shared/types/index.ts
@@ -3,9 +3,12 @@ import { TTask } from './Task';
 import { TInput } from './Input';
 import { TModalProps } from './Modal';
 import { TTasksList } from './TasksList';
+import { THttpMethod } from './HttpMethod';
+import { TLoginFormProps } from './LoginForm';
 import { TTaskCreateProps } from './TaskCreate';
 import { TUseFormReturn } from './useFormReturn';
 import { TResponseError } from './ResponseError';
+import { TClickToActionProps } from './ClickToAction';
 import { TTaskCreateFormProps } from './TaskCreateForm';
 import { TTasksListSideBarProps } from './TasksListSidebar';
 import { TaskDetailsSidebarProps } from './TaskDetailsSidebar';
@@ -15,10 +18,13 @@ export type {
   TTask,
   TInput,
   TTasksList,
+  THttpMethod,
   TModalProps,
   TResponseError,
   TUseFormReturn,
+  TLoginFormProps,
   TTaskCreateProps,
+  TClickToActionProps,
   TTaskCreateFormProps,
   TTasksListSideBarProps,
   TaskDetailsSidebarProps,
