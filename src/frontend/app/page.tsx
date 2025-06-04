45cae9ad5bd5376636f361a0b26e5b9b5dcb7746
Yohan William DUNON <yoh.d.tech@proton.me>
2025-06-04 12:14:41 +0200

    [feat]: Update login page
    - Update imports entities
    - Use reusable components and functions
    
    [Issue: #]

--- src/frontend/app/page.tsx
+++ src/frontend/app/page.tsx
@@ -1,60 +1,25 @@
 'use client';
-import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import Link from 'next/link';
-
-interface ErrorResponse {
-  message?: string;
-}
+import { Types, Schemas, Components, Hooks, Functions } from '@/shared';
 
 export default function Login() {
   const router = useRouter();
+  const { values, onChange, validate, reset, errors } =
+    Hooks.useForm<Types.TUser>({
+      initialValues: {
+        email: '',
+        password: '',
+      },
+      schema: Schemas.userSchema,
+    });
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  Hooks.useAuthRedirect();
 
-  useEffect(() => {
-    async function checkAuth() {
-      const res = await fetch('http://localhost:3000/auth/me', {
-        method: 'GET',
-        credentials: 'include',
-      });
-
-      if (res.ok) {
-        router.replace('/home');
-      }
-    }
-
-    checkAuth().catch(console.error);
-  }, [router]);
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    try {
-      const res = await fetch('http://localhost:3000/auth/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, password }),
-        credentials: 'include',
-      });
-
-      if (!res.ok) {
-        const error = (await res.json()) as ErrorResponse;
-        if (error && typeof error.message === 'string') {
-          alert('Login failed: ' + error.message);
-        } else {
-          alert('Login failed: unknown error');
-        }
-        return;
-      }
-
-      router.push('/home');
-    } catch (err) {
-      alert('Login error: ' + (err as Error).message);
-    }
-  };
+  async function handleSubmit() {
+    if (!validate()) return;
+    const response = await Functions.handleAuth('/auth/login', values, reset);
+    if (response) router.push('/home');
+  }
 
   return (
     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
@@ -66,71 +31,20 @@ export default function Login() {
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form
-          className="space-y-6"
-          onSubmit={(e) => {
-            void handleSubmit(e);
+        <Components.LoginForm
+          values={values}
+          onChange={onChange}
+          errors={errors}
+          handleSubmit={() => {
+            void handleSubmit();
           }}
-        >
-          <div>
-            <label
-              htmlFor="email"
-              className="block text-sm/6 font-medium text-gray-900"
-            >
-              Adresse e-mail
-            </label>
-            <div className="mt-2">
-              <input
-                type="email"
-                name="email"
-                id="email"
-                autoComplete="email"
-                required
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-              />
-            </div>
-          </div>
-          <div>
-            <label
-              htmlFor="password"
-              className="block text-sm/6 font-medium text-gray-900"
-            >
-              Mot de passe
-            </label>
-            <div className="mt-2">
-              <input
-                type="password"
-                name="password"
-                id="password"
-                autoComplete="current-password"
-                required
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-              />
-            </div>
-          </div>
-          <div>
-            <button
-              type="submit"
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Connectez vous
-            </button>
-          </div>
-        </form>
+        />
 
-        <p className="mt-10 text-center text-sm/6 text-gray-500">
-          Pas encore de compte ?&nbsp;
-          <Link
-            href="/register"
-            className="font-semibold text-indigo-600 hover:text-indigo-500"
-          >
-            Créez un compte
-          </Link>
-        </p>
+        <Components.ClickToAction
+          route={`/register`}
+          question={`Pas encore de compte ?`}
+          action={`Créez un compte`}
+        />
       </div>
     </div>
   );
